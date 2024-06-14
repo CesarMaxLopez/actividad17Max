1fa28f13b4318d2e36ebd0a94d87a12bd77eabd6
CesarMaxLopez <160643427+CesarMaxLopez@users.noreply.github.com>
2024-06-14 01:42:34 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>Act.17</title>
+  </head>
+  <body>
+    <?php 
+    echo '<h1>La Act. 17</h1><br>';
+    echo '<table>
+         <tr>
+         <td>César Maximiliano López Cuevas</td>
+         </tr></table>';
+    echo '<p>Colonia: Patito Suave</p>';
+    echo '<p>Calle: El pichucho</p>';
+    echo '<p>Numero de casa: #1106</p>';
+    echo '<p>Municipio: GotamCity</p>';
+    echo '<p>Telefono: 881-978-2345</p>';
+    ?> 
+    
+
+</html>
---
+++ readme.txt
@@ -0,0 +1,9 @@
+Iniciar sesion 
+Imprimir cmo titulo "La Act. 17"
+Imprimir nombre "César Maximiliano López Cuevas"
+Imprimir colonia "Patito Suave"
+Imprimir calle "El pichucho"
+Imprimir no. de casa "#1106"
+Imprimir municipio "GotamCity"
+Imprimir telefono "881-978-2345"
+Finalizar sesion 
